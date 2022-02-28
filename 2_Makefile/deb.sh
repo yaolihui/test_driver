567627a3f3e54b01429024091c1d3a8db467cce9
yaolihui <yaolihui888@163.com>
2022-02-28 20:13:20 +0800

    apt install

---
+++ 2_Makefile/deb.sh
@@ -0,0 +1,2 @@
+sudo apt install -y 	python make g++ gcc gcc-arm-linux-gnueabi gcc-aarch64-linux-gnu bison flex libssl-dev libcurl4-openssl-dev libsdl2-dev libelf-dev libncurses-dev genisoimage \
+						openssh-server openssh-client tree pigz ccache  net-tools 
