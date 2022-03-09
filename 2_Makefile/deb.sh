10308008e3271420633327984ddb48bbcd9a34bb
yaolihui <yaolihui888@163.com>
2022-03-09 17:30:43 +0800

    upate Makefile

--- 2_Makefile/deb.sh
+++ 2_Makefile/deb.sh
@@ -1,2 +1,1 @@
-sudo apt install -y 	python make g++ gcc gcc-arm-linux-gnueabi gcc-aarch64-linux-gnu bison flex libssl-dev libcurl4-openssl-dev libsdl2-dev libelf-dev libncurses-dev genisoimage \
-						openssh-server openssh-client tree pigz ccache  net-tools 
+sudo apt install -y 	python make g++ gcc gcc-arm-linux-gnueabi gcc-aarch64-linux-gnu bison flex libssl-dev libcurl4-openssl-dev libsdl2-dev libelf-dev libncurses-dev genisoimage openssh-server openssh-client tree pigz ccache net-tools samba samba-common
