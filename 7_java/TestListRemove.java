3c847f89d265c27d953d81ff1ed8b5d532db6ba9
yaolihui <yaolihui888@163.com>
2022-09-13 09:07:32 +0800

    TestListRemove

--- 7_java/TestListRemove.java
+++ 7_java/TestListRemove.java
@@ -6,20 +6,20 @@ public class TestListRemove {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (int i =0; i< 10; i++) {
+		
+		for (int i =0; i < 10; i++) {
 			list.add(new Integer(i));
 		}
 		
-		for (int i =0; i< 10; i++) {
-			if (list.contains(8)) {
-				list.remove(8);
-				// i--;
-			}
+		for (int i =0; i < list.size(); i++) {
+			Integer e = list.get(i);
+			list.remove(e);
+			//i--;
+			System.out.println("remove:" + i + ",size:" + list.size());
 		}
 		
 		for(Integer i: list) {
 			System.out.println(i);
 		}
-		
 	}
 }
