a9e1a33809f6aff1f08463be3c27375fc41507c6
yaolihui <yaolihui888@163.com>
2022-09-15 12:22:36 +0800

    impl TestBiFunction.java

--- 7_java/java.util.function/TestBiFunction.java
+++ 7_java/java.util.function/TestBiFunction.java
@@ -1,13 +1,29 @@
 import java.util.*;
 import java.util.function.*;
 
-public class TestPredicate {
-	
-	
+public class TestBiFunction{
+
 	public static void main(String[] args) {
 		System.out.println("Hollo World!");
-		
-		
+
+		BiFunction<Integer, Integer, Integer> bf1 = (a, b) -> a + b;
+		System.out.println("result:" + bf1.apply(3, 4));
+
+		BiFunction<String, Integer, String> bf2 = (a, b) -> a + b;
+		System.out.println("result:" + bf2.apply("3", 4));
+
+		BiFunction<Integer, Integer, String> bf3 = (a, b) -> a + b + "";
+		System.out.println("result:" + bf3.apply(3, 4));
+
+		BiFunction<Integer, Integer, String> bf4 = (a, b) -> "" + a + b;
+		System.out.println("result:" + bf4.apply(3, 4));
+
 	}
-	
+
 }
+/*
+result:7
+result:34
+result:7
+result:34
+*/
