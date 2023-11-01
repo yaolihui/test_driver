b38dc1060ac2bc4647e714b54738191515860f2a
yaolihui <yaolihui888@163.com>
2023-11-01 19:57:17 +0800

    new PatchViewer.html

--- 2_misc/wsl-mount.bat
+++ 2_misc/wsl-mount.bat
@@ -1,4 +1,5 @@
 :loop
 ping 127.0.0.1 -n 1 > nul
-c:\windows\system32\wsl.exe --mount \\.\PHYSICALDRIVE1
+::echo GET-CimInstance -query "SELECT * from Win32_DiskDrive"
+c:\windows\system32\wsl.exe --mount \\.\PHYSICALDRIVE0
 if %errorlevel% == 0 goto loop
