ba0b108bcadd25648501e643faeab5527b80341b
yaolihui <yaolihui@xdja.com>
2024-06-13 17:21:37 +0800

    mmap offset

--- 6_c/mmap.c
+++ 6_c/mmap.c
@@ -9,13 +9,15 @@ typedef struct{
 	char string[12];
 }RECORD;
  
-#define NRECORDS (100)
+#define NRECORDS (256 + 1) // 4096 + 16
  
 int main()
 {
 	RECORD record, *mapped;
-	int i, f;
+	int i, f, pagesize;
 	FILE *fp;
+	pagesize = sysconf(_SC_PAGESIZE);
+	printf("sizeof(RECORD)=%ld, pagesize=%d\n", sizeof(RECORD), pagesize);
 	
 	fp = fopen("records.dat", "w+");
 	for( i = 0; i < NRECORDS; i++)
@@ -23,7 +25,7 @@ int main()
 		record.integer = i;
 		sprintf(record.string, "[RECORD-%d]", i);
 		fwrite(&record, sizeof(record), 1, fp);
-		}
+	}
 	fclose(fp);
 	
 	fp = fopen("records.dat", "r+");
@@ -37,10 +39,12 @@ int main()
 	fclose(fp);
 	
 	f = open("records.dat", O_RDWR);
-	mapped = (RECORD*)mmap(0, (NRECORDS + 10) * sizeof(record), 
+	mapped = (RECORD*)mmap(NULL, (NRECORDS + 10) * sizeof(record), 
 	                        PROT_READ | PROT_WRITE, MAP_SHARED, f, 0);
-	
-	printf("f:[%d]\n", f);
+	printf("[0]: integer=%d, %s\n", mapped[0].integer, mapped[0].string);
+
+	off_t size = lseek(f, 0, SEEK_END);
+	printf("f:[%d], filesize:%ld\n", f, size);
 	//open是系统调用，返回文件描述符。fopen是库函数，返回指针。                        
 	mapped[43].integer = 343;
 	sprintf(mapped[43].string, "[RECORD-%d]", mapped[43].integer);
@@ -51,7 +55,13 @@ int main()
 
 	msync((void *) mapped, NRECORDS * sizeof(record), MS_ASYNC);
 	munmap((void *)mapped, NRECORDS * sizeof(record));
+	//close(f);
+
+	//f = open("records.dat", O_RDWR);
+        mapped = (RECORD*)mmap(mapped, NRECORDS * sizeof(record),
+                                PROT_READ | PROT_WRITE, MAP_SHARED, f, pagesize/*必须整数倍*/);
+	printf("[43]: integer=%d, string=%s\n", mapped[0].integer, mapped[0].string);
+	munmap((void *)mapped, NRECORDS * sizeof(record));
 	close(f);
-	
 	return 0;	
 }
